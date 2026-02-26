5018c3354173de6c4f91195480dcc71ce8d48072
siddharth shukla <siddharthshukla.cs24@bmsce.ac.in>
2026-02-26 09:49:56 +0530

    Update linearSearch.c

--- lab1/linear search/linearSearch.c
+++ lab1/linear search/linearSearch.c
@@ -1,1 +1,10 @@
-
+int missingNumber(int* nums, int numsSize) {
+    int c;
+    c=numsSize;
+    int sum=(c*(c+1))/2;
+    int missing=sum;
+    for(int i=0;i<numsSize;i++) {
+        missing=missing-nums[i];
+    }
+    return missing;
+}
