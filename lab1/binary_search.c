738dd0f4c08f42848ac98af3cdcbfe35f09b1733
siddharth shukla <siddharthshukla.cs24@bmsce.ac.in>
2026-02-26 09:31:05 +0530

    Correct binary search logic in search function
    Fix binary search implementation to correctly update indices.

--- lab1/binary_search.c
+++ lab1/binary_search.c
@@ -1,1 +1,20 @@
+//leetCode 704
+int search(int* nums, int numsSize, int target) {
+    int l=0;
+    int r=numsSize-1;
 
+    while(l!=r) {
+        int mid=(l+r)/2;
+        if(target==nums[mid]) {
+            return mid;
+        }
+        else if(target>nums[mid]) {
+            l=mid+1;
+        }
+        else if(target<nums[mid]) {
+            r=mid-1;
+        }
+    }
+    return -1;
+    
+}
